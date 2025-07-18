d00ea465fdba5a8a76c218760aa4971cdabe4f40
Blake Williams <blake.williams@ssoready.com>
2025-07-17 21:59:24 -0700

    Add .env logic to script

--- bin/install-and-run.sh
+++ bin/install-and-run.sh
@@ -5,5 +5,10 @@ git clone git@github.com:tesseral-labs/client-example.git
 
 cd client-example
 
+echo "Creating .env file..."
+cat <<EOF > .env
+TESSERAL_PUBLISHABLE_KEY=$0
+EOF
+
 echo "Running the example application..."
 make dev
